24937d6bd937b277ee7407f4b8921b8b95873686
edpyt <edrsa1321s@gmail.com>
2023-11-22 19:18:04 +0300

    HTML main page now generates from template

--- chapter3/functional_tests.py
+++ chapter3/functional_tests.py
@@ -34,7 +34,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows)
+            any(row.text == '1: Купить павлиньи перья' for row in rows),
+            'Новый элемент списка не появился в таблице'
         )
 
         self.fail('Закончить тест!')
