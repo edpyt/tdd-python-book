cd09c33fcfd581473d763cb68b50585c4696899a
edpyt <edrsa1321s@gmail.com>
2023-11-22 19:04:23 +0300

    refactor home page view for using templates

--- chapter3/depends.py
+++ chapter3/depends.py
@@ -1,12 +1,6 @@
 import sqlite3
 from typing import Generator
 
-from fastapi.templating import Jinja2Templates
-
-
-def get_templates():
-    return Jinja2Templates(directory='templates')
-
 
 def get_db_connection() -> Generator:
     yield sqlite3.connect('db.sqlite3')
--- chapter3/main.py
+++ chapter3/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
-from superlists.routes import router as superlists_router
+from lists.routes import router as superlists_router
 
 app = FastAPI()
-
 
 app.include_router(superlists_router)
