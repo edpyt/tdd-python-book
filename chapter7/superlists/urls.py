3f02c085fe61deff5b11674e0c57250a1f805449
edpyt <edrsa1321s@gmail.com>
2023-11-28 18:30:52 +0300

    new URL, view and template to display list

--- chapter7/superlists/urls.py
+++ chapter7/superlists/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from lists.views import home_page
+from lists.views import home_page, view_list
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('lists/unique-list/', view_list, name='view_list')
 ]
