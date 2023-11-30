80038170e288e654fb8d758c574a76c5b5134dba
edpyt <edrsa1321s@gmail.com>
2023-11-30 18:18:43 +0300

    using `Bootstrap` for improve macket

--- chapter8/functional_tests/tests.py
+++ chapter8/functional_tests/tests.py
@@ -1,6 +1,7 @@
 
 import time
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -10,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -107,14 +108,9 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
+            inputbox.location["x"] + inputbox.size["width"] / 2,
             512,
-            delta=10
+            delta=10,
         )
